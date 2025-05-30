f6a302ab3c49dacb655f7f6765b5cab3b44aba51
Alvin Kamau <alvin.kamau1 @student.moringaschool.com>
2025-05-30 11:23:01 +0300

    Rename students table to scholars, manual migration, update Alembic env

--- migrations/env.py
+++ migrations/env.py
@@ -18,8 +18,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib')))
 from models import Base
+
 target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -59,7 +64,7 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
